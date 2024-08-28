ea70ae1cb192ee366962430e6c9d5446742064d8
Yovanny Diaz <thebigyovadiaz@gmail.com>
2024-08-27 23:35:05 -0400

    mocking test countdown update

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"github.com/thebigyovadiaz/testing_go/src/mocking"
 	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Learning TDD with Go")
 	// _ = http.ListenAndServe(":5001", http.HandlerFunc(dependency_injection.MyGreetHandler))
 
-	sleeper := &mocking.DefaultSleeper{}
+	sleeper := &mocking.ConfigurableSleeper{Duration: 1 * time.Second, Slept: time.Sleep}
 	mocking.Countdown(os.Stdout, sleeper)
 }
--- src/mocking/mocking.go
+++ src/mocking/mocking.go
@@ -9,20 +9,9 @@ import (
 const (
 	finalWord      = "Go!"
 	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
 )
-
-func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
-		fmt.Fprintln(out, i)
-		sleeper.Sleep()
-	}
-
-	fmt.Fprint(out, finalWord)
-}
-
-type Sleeper interface {
-	Sleep()
-}
 
 type SpySleeper struct {
 	Calls int
@@ -36,4 +25,47 @@ type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+type ConfigurableSleeper struct {
+	Duration time.Duration
+	Slept    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.Slept(c.Duration)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
 }
