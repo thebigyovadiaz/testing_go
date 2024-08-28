fcf026626b9a08da915546d18d2d5e1a8735712d
Yovanny Diaz <thebigyovadiaz@gmail.com>
2024-08-27 22:32:46 -0400

    mocking test countdown

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	dependency_injection "github.com/thebigyovadiaz/testing_go/src/dependency-injection"
-	"net/http"
+	"github.com/thebigyovadiaz/testing_go/src/mocking"
+	"os"
 )
 
 func main() {
 	fmt.Println("Learning TDD with Go")
-	_ = http.ListenAndServe(":5001", http.HandlerFunc(dependency_injection.MyGreetHandler))
+	// _ = http.ListenAndServe(":5001", http.HandlerFunc(dependency_injection.MyGreetHandler))
+
+	sleeper := &mocking.DefaultSleeper{}
+	mocking.Countdown(os.Stdout, sleeper)
 }
---
+++ src/mocking/mocking_test.go
@@ -0,0 +1,28 @@
+package mocking
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	t.Run("test iterate buffer", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := SpySleeper{}
+
+		Countdown(buffer, &spySleeper)
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		}
+	})
+}
